7d16ba6d7c52a832df9cc9f07302134ef53b0f9d
Kevin Beach <kevin.beach@hotmail.co.uk>
2014-10-16 00:43:52 +0100

    Initial commit

---
+++ app/src/main/java/com/gosto/kbeach/emmtek_locate/Locate.java
@@ -0,0 +1,140 @@
+package com.gosto.kbeach.emmtek_locate;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class Locate extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locate);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new LocateFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.locate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id==R.id.action_phonenumber){
+            //ADD IN
+            // Handlers to change the phone number preference
+
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            final PhoneConfigDialog phoneConfigDialog = new PhoneConfigDialog();
+            phoneConfigDialog.show(fragmentManager,"phoneconfig");
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class LocateFragment extends Fragment {
+        private Button mLocate;
+        private WebView mMapWebView;
+        private SharedPreferences sharedPreferences;
+        public LocateFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_locate, container, false);
+            mLocate = (Button) rootView.findViewById(R.id.buttonLocate);
+            mMapWebView = (WebView) rootView.findViewById(R.id.webViewLocate);
+            sharedPreferences = getActivity().getSharedPreferences("LocateData", Context.MODE_PRIVATE);
+            final String mURL = sharedPreferences.getString("mapaddress","https://www.google.co.uk/maps/@54.8736587,-4.8013245,6z?hl=en");
+            //Add in new client with override URL Loading to prevent browser selection
+            mMapWebView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    return false;
+                }
+            });
+            //Set JavaScript to be enabled for google maps.
+            WebSettings webSettings = mMapWebView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            mMapWebView.loadUrl(mURL);
+
+            //add in the shared preferences listener to update the map on changes to shared preference
+            // this is a bit clunky - change to using the broadcast listener SMS inside the fragment
+            // enabled after locate is pressed and then pass data as per fragments.
+
+            SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                    //Toast.makeText(getActivity(), "preference changed", Toast.LENGTH_SHORT).show();
+                    if (s.equals("mapaddress")){
+                        //Toast.makeText(getActivity(), "mapaddress changed", Toast.LENGTH_SHORT).show();
+                        final String mURL = sharedPreferences.getString("mapaddress","https://www.google.co.uk/maps/@54.8736587,-4.8013245,6z?hl=en");
+                        mMapWebView.loadUrl(mURL);
+                    }
+                }
+            };
+
+            sharedPreferences.registerOnSharedPreferenceChangeListener(listener);
+
+
+            mLocate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(),"button clicked",Toast.LENGTH_LONG).show();
+                    String mPhoneNumber;
+                    String mSmsText = new String("LOCATE");
+                    mPhoneNumber = sharedPreferences.getString("phone","Default");
+                    if (mPhoneNumber.equals("Default")){
+                        Toast.makeText(getActivity(),"Set EmmTek Phone Number",Toast.LENGTH_LONG).show();
+
+                    }
+                    else{
+                        SmsSend smsSend = new SmsSend(mPhoneNumber,mSmsText);
+                        if (smsSend.Send()){
+                            Toast.makeText(getActivity(), "Your sms has successfully sent!",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(getActivity(), "Your sms has failed...",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+            });
+            return rootView;
+        }
+    }
+}
---
+++ app/src/main/java/com/gosto/kbeach/emmtek_locate/SmsReceiver.java
@@ -0,0 +1,77 @@
+package com.gosto.kbeach.emmtek_locate;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+/**
+ * Created by kbeach on 23/09/2014.
+ */
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String messageReceived = "";
+        if(bundle != null)
+        {
+            // retrieve SMS received
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i=0; i<msgs.length; i++){
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                messageReceived += msgs[i].getMessageBody().toString();
+                messageReceived += "\n";
+            }
+            // get the telephone number from preferences
+            SharedPreferences sharedPreferences = context.getSharedPreferences("LocateData",Context.MODE_PRIVATE);
+            String mPhoneNumber;
+            mPhoneNumber = sharedPreferences.getString("phone","Default");
+            String senderPhoneNumber=msgs[0].getOriginatingAddress();
+            // Test toasts to verify phone numbers
+            //Toast.makeText(context, "senderPhoneNumber " + senderPhoneNumber, Toast.LENGTH_SHORT).show();
+            //Toast.makeText(context, "mPhoneNumber " + mPhoneNumber, Toast.LENGTH_SHORT).show();
+            // Check the phone numbers are greater than 10 digits
+            if (mPhoneNumber.length() > 10) {
+
+                if (senderPhoneNumber.length() > 10) {
+
+                    //Use only the last 10 digits to allow both +44 and 0 to register as the same phone number
+                    // This will only work in the UK! Looks to see if you can remove the country code??
+                    // Add in error traps as well
+
+                    if (mPhoneNumber.substring(mPhoneNumber.length() - 10).equals(senderPhoneNumber.substring(senderPhoneNumber.length() - 10))) {
+
+                        //display SMS message
+                        Toast.makeText(context, messageReceived, Toast.LENGTH_SHORT).show();
+                        //get the HTTP address from message - strCheck validates address
+                        String strCheck = context.getString(R.string.http_ref_string);
+                        if(messageReceived.indexOf(strCheck) > -1) {
+                            String mapAddress = messageReceived.substring(messageReceived.indexOf(strCheck));
+                            Toast.makeText(context, mapAddress, Toast.LENGTH_LONG).show();
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("mapaddress",mapAddress);
+                            editor.commit();
+
+                        }else{
+                            Toast.makeText(context, "No HTTP address", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } else {
+                        Toast.makeText(context, "Different phone number", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            //get the sender phone number
+
+
+        }
+    }
+}
